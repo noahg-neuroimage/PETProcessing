9dee027dbc7530e8f8e2acaea6bd4998f79698f4
noahg-neuroimage <noahg@wustl.edu>
2024-01-31 15:08:46 -0600

    Extremely basic pet motion correction module. Function ``calculate_pet_motion_correction`` takes a 4D pet image in the form of an np.ndarray, applies the basic Ants motion correction algorithm with no additional arguments, and returns the motion corrected pet image as an np.ndarray, as well as the motion parameters and field displacement.

---
+++ pet_moco/pet_moco.py
@@ -0,0 +1,47 @@
+"""
+A module for performing basic motion correction on 4D PET scans.
+
+Example:
+    python pet_moco.py -i pet.nii -o pet_mc.nii --cost_func normcorr
+
+Attributes:
+
+Todo:
+    * Write motion correction module
+    * Test motion correction module
+"""
+import numpy as np
+import numba
+import nibabel
+import ants
+
+
+def calculate_pet_motion_correction(pet_image_series: np.ndarray,
+                                    ) -> np.ndarray:
+    """
+    Takes an input 4D PET reconstructed time series image, 
+    calculates the motion correction, and returns the motion
+    corrected image.
+
+    Args:
+        pet_image_series (np.ndarray): Input 4D PET image series to be motion corrected.
+        
+    Returns:
+        pet_image_moco (np.ndarray): The motion corrected 4D PET image.
+    
+    """
+    pet_image_ants = ants.from_numpy(pet_image_series)
+    pet_image_moco, moco_params, moco_fd = ants.motion_correction(pet_image_ants)
+    return pet_image_moco, moco_params, moco_fd
+
+
+def write_pet_motion_correction(pet_file: str, moco_output_file: str, **kwargs) -> int:
+    """
+    Function to call ``calculate_pet_motion_correction`` PET motion
+    correction function, and write result to file.
+
+    Args:
+        pet_file (str): Path to input 4D PET image file
+        moco_output_file (str): Path to output motion corrected PET file
+    """
+    return 0
