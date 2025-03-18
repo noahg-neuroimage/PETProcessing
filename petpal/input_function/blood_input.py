4df1958af4b4af9416636a6fa9ce86e87dc8c070
Noah Goldman <noahg@wustl.edu>
2025-03-18 15:23:30 -0500

    chore: remove comment

--- petpal/input_function/blood_input.py
+++ petpal/input_function/blood_input.py
@@ -178,7 +178,6 @@ def resample_blood_data_on_scanner_times(blood_tac_path: str,
     assert rescale_constant > 0.0, "Rescale constant must be greater than zero."
     image_meta_data = image_io.load_metadata_for_nifti_with_same_filename(image_path=reference_4dpet_img_path)
     frame_times = np.asarray(image_meta_data['FrameReferenceTime']) / 60.0
-    # blood_times, blood_activity = image_io.safe_load_tac(filename=blood_tac_path)
     blood_tac = TimeActivityCurve.from_tsv(tac_path=blood_tac_path)
     blood_intp = BloodInputFunction(time=blood_tac.tac_times_in_minutes, activity=blood_tac.tac_vals, thresh_in_mins=lin_fit_thresh_in_mins)
     resampled_blood = blood_intp.calc_blood_input_function(t=frame_times)
