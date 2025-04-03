620d43eb3305c676c3a19cf7bca5d30f11bebf7b
noahg-neuroimage <noahg@wustl.edu>
2025-04-03 13:24:10 -0500

    feat: linear, log, both options added to TacFigure. plot_tacs_in_regions_list now works as intended. plot_regional_tacs fixed plotting errorbar plot over line plot. Added custom color option for regional plots. Added legend to regional plots.

--- petpal/visualizations/tac_plots.py
+++ petpal/visualizations/tac_plots.py
@@ -4,10 +4,11 @@ Simple module to plot TACs from a TACs folder created by petpal function write-t
 import glob
 import os
 import matplotlib.pyplot as plt
+from matplotlib import colormaps
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from ..utils.time_activity_curve import TimeActivityCurve, MultiTACAnalysisMixin
+from ..utils.time_activity_curve import MultiTACAnalysisMixin
 
 def tacs_to_df(tacs_dir: str,
                participant: str):
@@ -91,7 +92,8 @@ class TacFigure:
     def __init__(self,
                  figsize: tuple = (8, 4),
                  xlabel: str = r'$t$ [minutes]',
-                 ylabel: str = r'TAC [$\mathrm{kBq/ml}$]'):
+                 ylabel: str = r'TAC [$\mathrm{kBq/ml}$]',
+                 plot_type: str='both'):
         r"""
         Initialize the TacFigure with two subplots, one with a linear scale and the other with a
         semi-logarithmic scale.
@@ -100,22 +102,59 @@ class TacFigure:
             figsize (tuple): The total size of the figure. Defaults to an 8x4 inches figure.
             xlabel (str): The label for the x-axis. Defaults to '$t$ [minutes]'.
             ylabel (str): The label for the y-axis. Defaults to 'TAC [$\mathrm{kBq/ml}$]'.
+            plot_type (str): Type of plot, with options 'linear', 'log', or 'both'.
         """
-        self.fig, self.axes = self.setup_linear_and_log_subplot(figsize=figsize)
-        self.fax = self.axes.flatten()
+        if plot_type=='both':
+            self.setup_linear_and_log_subplot(xlabel=xlabel, ylabel=ylabel, figsize=figsize)
+        elif plot_type=='linear':
+            self.setup_linear_subplot(xlabel=xlabel, ylabel=ylabel, figsize=figsize)
+        elif plot_type=='log':
+            self.setup_log_subplot(xlabel=xlabel, ylabel=ylabel, figsize=figsize)
+        else:
+            raise ValueError(f"Got unexpected plot_type {plot_type}, "
+                             "expected one of: 'linear', 'log', 'both'.")
+
+    def setup_linear_subplot(self, xlabel: str, ylabel: str, figsize: tuple):
+        """
+        Get the figure and axes objects for a 1x1 MatPlotLib subplot.
+
+        Args:
+            figsize (tuple): Size of the figure.
+        """
+        subplot = plt.subplots(1, 1, sharey=True, constrained_layout=True, figsize=figsize)
+        self.fig, self.axes = subplot
+        self.fax = [self.axes]
         _xlabel_set = [ax.set(xlabel=xlabel) for ax in self.fax]
         self.fax[0].set(ylabel=ylabel, title='Linear')
-        self.fax[1].set(xscale='log', title='SemiLog-X')
 
 
-    def setup_linear_and_log_subplot(self, figsize: tuple):
+    def setup_log_subplot(self, xlabel: str, ylabel: str, figsize: tuple):
+        """
+        Get the figure and axes objects for a 1x1 MatPlotLib subplot.
+
+        Args:
+            figsize (tuple): Size of the figure.
+        """
+        subplot = plt.subplots(1, 1, sharey=True, constrained_layout=True, figsize=figsize)
+        self.fig, self.axes = subplot
+        self.fax = [self.axes]
+        _xlabel_set = [ax.set(xlabel=xlabel) for ax in self.fax]
+        self.fax[0].set(xscale='log',ylabel=ylabel, title='SemiLog-X')
+
+
+    def setup_linear_and_log_subplot(self, xlabel: str, ylabel: str, figsize: tuple):
         """
         Get the figure and axes objects for a 1x2 MatPlotLib subplot.
 
         Args:
             figsize (tuple): Size of the figure.
         """
-        return plt.subplots(1, 2, sharey=True, constrained_layout=True, figsize=figsize)
+        subplot = plt.subplots(1, 2, sharey=True, constrained_layout=True, figsize=figsize)
+        self.fig, self.axes = subplot
+        self.fax = self.axes.flatten()
+        _xlabel_set = [ax.set(xlabel=xlabel) for ax in self.fax]
+        self.fax[0].set(ylabel=ylabel, title='Linear')
+        self.fax[1].set(xscale='log', title='SemiLog-X')
 
 
     def add_tac(self, tac_times: np.ndarray, tac_vals: np.ndarray, **kwargs):
@@ -162,43 +201,60 @@ class RegionalTacFigure(TacFigure,MultiTACAnalysisMixin):
     Handle plotting regional TACs generated with PETPAL.
     """
     def __init__(self,
-                 tacs_dir: str, 
+                 tacs_dir: str,
                  figsize: tuple = (8, 4),
                  xlabel: str = r'$t$ [minutes]',
-                 ylabel: str = r'TAC [$\mathrm{kBq/ml}$]'):
+                 ylabel: str = r'TAC [$\mathrm{kBq/ml}$]',
+                 plot_type='both'):
         MultiTACAnalysisMixin.__init__(self,input_tac_path='',tacs_dir=tacs_dir)
-        TacFigure.__init__(self,figsize=figsize,xlabel=xlabel,ylabel=ylabel)
+        TacFigure.__init__(self,figsize=figsize,xlabel=xlabel,ylabel=ylabel,plot_type=plot_type)
 
     @property
-    def tacs_objects_list(self):
-        return self.get_tacs_objects_list_from_files_list(self.tacs_files_list)
+    def tacs_objects_dict(self):
+        """
+        Placeholder
+        """
+        return self.get_tacs_objects_dict_from_dir(self.tacs_dir)
 
 
     def get_figure(self):
+        """
+        Placeholder
+        """
         return self.fig
 
 
-    def plot_regional_tacs(self):
+    def plot_regional_tacs(self,show_legend: bool=False):
         """
         Placeholder
         """
-        tacs_obj_list = self.tacs_objects_list()
-        for tac in tacs_obj_list:
-            self.add_tac(tac_times=tac.times, tac_vals=tac.activity)
+        colors = colormaps['Dark2'].colors
+        tacs_obj_dict = self.tacs_objects_dict
+        regions = list(tacs_obj_dict.keys())
+        for i, tac in enumerate(tacs_obj_dict.values()):
             self.add_errorbar(tac_times=tac.times,
                               tac_vals=tac.activity,
-                              uncertainty=tac.uncertainty)
+                              uncertainty=tac.uncertainty,
+                              color=colors[i%len(colors)],
+                              label=regions[i])
+        if show_legend:
+            self.gen_legend()
         return self.get_figure()
 
 
-    def plot_tacs_in_regions_list(self,regions: list[str | int]):
+    def plot_tacs_in_regions_list(self,regions: list[str | int], show_legend: bool=False):
         """
         Placeholder
         """
-        tacs_obj_list = self.tacs_objects_list()
-        for region in regions:
-            tac = tacs_obj_list[region]  # NOTE: THIS DOESNT DO ANYTHING
+        colors = colormaps['Dark2'].colors
+        tacs_obj_dict = self.tacs_objects_dict
+        for i, region in enumerate(regions):
+            tac = tacs_obj_dict[region]
             self.add_errorbar(tac_times=tac.times,
                               tac_vals=tac.activity,
-                              uncertainty=tac.uncertainty)
+                              uncertainty=tac.uncertainty,
+                              label=region,
+                              color=colors[i%len(colors)])
+        if show_legend:
+            self.gen_legend()
         return self.get_figure()
