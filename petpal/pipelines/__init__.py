f2ddc114cc0dea8fc111507b96cd5d475e3c103b
Furqan Dar <darf@kenyon.edu>
2025-04-22 12:31:13 -0500

    Added docs to the pca guided idif steps

--- petpal/pipelines/__init__.py
+++ petpal/pipelines/__init__.py
@@ -2,6 +2,7 @@ from . import steps_base
 from . import steps_containers
 from . import preproc_steps
 from . import kinetic_modeling_steps
+from . import pca_guided_idif_steps
 from . import pipelines
 
 def main():
--- petpal/pipelines/pca_guided_idif_steps.py
+++ petpal/pipelines/pca_guided_idif_steps.py
@@ -1,3 +1,49 @@
+r"""
+This module provides classes and utilities for computing Image-Derived Input Functions
+(IDIF) using a PCA-guided approach. It is designed to integrate into preprocessing pipelines
+and offers functionality for optimizing voxel selection, applying fitting techniques,
+and selecting top-ranked voxels from PCA components.
+
+Classes:
+--------
+
+1. :class:`PCAGuidedIDIFMixin`:
+    - Acts as a mixin to provide shared functionality across PCA-guided steps.
+    - Manages input/output paths, initializes configurations, and helps connect pipeline steps.
+    - Provides methods to infer output paths and set input paths dynamically.
+
+2. :class:`PCAGuidedFitIDIFStep`:
+    - Represents a pipeline step for optimizing voxel selection to fit the IDIF using PCA-guided methods.
+    - Extends both :class:`~.ObjectBasedStep` and :class:`~.PCAGuidedIDIFMixin` to integrate optimization fitting into a pipeline.
+    - Includes parameters to control optimization behavior, such as:
+        - **alpha**: Weight for the peak term to encourage a high TAC peak.
+        - **beta**: Weight for the smoothness term to ensure low total variation.
+        - **method**: The optimization method for fitting.
+
+3. :class:`PCAGuidedTopVoxelsIDIFStep`:
+    - Represents a pipeline step to calculate the IDIF by selecting the top `N` voxels from a specific PCA component.
+    - Extends both :class:`~.ObjectBasedStep` and :class:`~.PCAGuidedIDIFMixin` for pipeline functionality.
+    - Allows customizing the selected PCA component and the number of voxels to include.
+
+Key Features:
+-------------
+- **Pipeline Integration**:
+    All classes are designed to be modular and compatible with preprocessing pipelines.
+    They inherit functionalities from :class:`~.ObjectBasedStep` and :class:`~.StepsAPI`.
+
+- **BIDS-Compatible Output**:
+    Output paths for TAC files are generated in a BIDS-like format using subject and session information
+    extracted from the input image paths.
+
+- **Optimization and Voxel Selection**:
+    Classes include functionality to:
+        - Select voxels based on PCA component ranking.
+        - Optimize voxel selection iteratively for better IDIF calculation.
+        - Use terms like smoothness, noise reduction, and peak emphasis for guiding optimization.
+
+"""
+
+
 from ..input_function import pca_guided_idif
 from ..pipelines.preproc_steps import ImageToImageStep
 from ..pipelines.steps_base import StepsAPI, ObjectBasedStep
@@ -5,6 +51,26 @@ from ..utils.bids_utils import gen_bids_like_filepath, parse_path_to_get_subject
 
 
 class PCAGuidedIDIFMixin(StepsAPI):
+    r"""Mixin class to provide shared functionality for PCA-guided IDIF pipeline steps.
+
+    This class is designed to standardize and extend functionality across various PCA-guided
+    IDIF calculation steps. It handles input and output path management, configuration initialization,
+    and helps define the relationships between preprocessing pipeline steps.
+
+    Inheritance:
+        Inherits from the :class:`~.StepsAPI` base class.
+
+    Attributes:
+        init_kwargs (dict): A dictionary containing initial configuration parameters.
+        call_kwargs (dict): A dictionary containing runtime call-specific parameters.
+        input_image_path (str): Path to the input 4D-PET image.
+        mask_image_path (str): Path to the mask image in the same space as 4D-PET image.
+        output_tac_path (str): Path where output TAC data will be saved.
+        num_pca_components (int): Number of PCA components to compute.
+        verbose (bool): Whether to show detailed diagnostic output or not.
+        name (str): Name of the pipeline step (default is "pca_guided_idif_base").
+
+    """
     def __init__(self,
                  input_image_path: str,
                  mask_image_path: str,
@@ -69,8 +135,8 @@ class PCAGuidedIDIFMixin(StepsAPI):
         self.init_kwargs['verbose'] = value
 
     def set_input_as_output_from(self, *sending_steps: ImageToImageStep) -> None:
-        """
-        Sets the input image paths based on the output paths from other steps in the pipeline.
+        """Sets the input image paths based on the output paths from other steps in the pipeline.
+
         The first sending step will set the input image path, and the second sending step will
         set the second image path.
 
@@ -97,8 +163,7 @@ class PCAGuidedIDIFMixin(StepsAPI):
                                   suffix: str = 'tac',
                                   ext: str = '.tsv',
                                   **extra_desc):
-        """
-        Infers the output array path based on the inputs and specified parameters.
+        r"""Infers the output array path based on the inputs and specified parameters.
 
         This method generates a BIDS-like derivatives filepath for the output based on the subject and
         session IDs extracted from the input image path.
@@ -109,6 +174,7 @@ class PCAGuidedIDIFMixin(StepsAPI):
             suffix (str, optional): Suffix for the output filename. Defaults to 'tac'.
             ext (str, optional): File extension for the output file. Defaults to '.tsv'.
             **extra_desc: Additional descriptive parameters for the output filename.
+
         """
         sub_id, ses_id = parse_path_to_get_subject_and_session_id(self.input_image_path)
         step_name_in_camel_case = snake_to_camel_case(self.name)
@@ -118,6 +184,38 @@ class PCAGuidedIDIFMixin(StepsAPI):
 
 
 class PCAGuidedFitIDIFStep(ObjectBasedStep, PCAGuidedIDIFMixin):
+    r"""Pipeline step for fitting the PCA-guided IDIF using an optimization-based approach.
+
+    This step extends the :class:`~.PCAGuidedIDIFMixin` for shared functionality and the :class:`~.ObjectBasedStep`
+    for pipeline execution. It uses the :class:`~.PCAGuidedIdifFitter` class to iteratively optimize voxel
+    selection for IDIF generation based on specified parameters.
+
+    Inheritance:
+        Inherits from :class:`~.ObjectBasedStep` and :class:`~.PCAGuidedIDIFMixin`.
+
+    Attributes:
+        alpha (float): Parameter weight for the peak term (emphasizes high TAC peaks).
+        beta (float): Parameter weight for the smoothness term (encourages smoother signals).
+        method (str): Optimization method to be used during fitting.
+
+    Example:
+
+        .. code-block:: python
+
+            from petpal.pipelines.pca_guided_idif_steps import PCAGuidedFitIDIFStep
+
+            step = PCAGuidedFitIDIFStep(input_image_path='/path/to/image.nii',
+                                        mask_image_path='/path/to/mask.nii',
+                                        output_array_path='/path/to/output.tsv',
+                                        num_pca_components=5,
+                                        verbose=True,
+                                        alpha=0.5,
+                                        beta=0.3,
+                                        method='ampgo')
+            ## Running and saving the results
+            step()  # Executes the step
+
+    """
     def __init__(self,
                  input_image_path: str,
                  mask_image_path: str,
@@ -128,6 +226,24 @@ class PCAGuidedFitIDIFStep(ObjectBasedStep, PCAGuidedIDIFMixin):
                  beta: float,
                  method: str,
                  **meth_kwargs):
+        r"""Initializes the PCA-guided fitting IDIF step.
+
+        Args:
+            input_image_path (str): Path to the input 4D-PET image.
+            mask_image_path (str): Path to the mask image in the same space as 4D-PET image.
+            output_array_path (str): Path where the resulting TAC data will be saved.
+            num_pca_components (int): Number of PCA components to consider.
+            verbose (bool): If `True`, enables detailed diagnostic output.
+            alpha (float): Parameter weight for the peak term.
+            beta (float): Parameter weight for the smoothness term.
+            method (str): Optimization method to use for fitting.
+            **meth_kwargs: Additional keyword arguments for the optimization method.
+
+        Side Effects:
+            - Initializes `python-guided-idif` fitter with the provided parameters.
+            - Populates `call_kwargs` with runtime-specific options for the fitting process.
+
+        """
         PCAGuidedIDIFMixin.__init__(self,
                                     input_image_path=input_image_path,
                                     mask_image_path=mask_image_path,
@@ -173,6 +289,37 @@ class PCAGuidedFitIDIFStep(ObjectBasedStep, PCAGuidedIDIFMixin):
 
 
 class PCAGuidedTopVoxelsIDIFStep(ObjectBasedStep, PCAGuidedIDIFMixin):
+    r"""Pipeline step for PCA-guided IDIF calculation using top-ranked voxels.
+
+    This step extends the :class:`~.PCAGuidedIDIFMixin` and provides functionality to select the top `N`
+    voxels from a specified PCA component for IDIF generation. It uses the
+    :class:`~.PCAGuidedTopVoxelsIDIF` class for its calculations.
+
+    Inheritance:
+        Inherits from :class:`~.ObjectBasedStep` and :class:`~.PCAGuidedIDIFMixin`.
+
+    Attributes:
+        selected_component (int): PCA component selected for voxel ranking.
+        num_of_voxels (int): Number of top-ranking voxels to select.
+
+    Example:
+
+        .. code-block:: python
+
+            from petpal.pipelines.pca_guided_idif_steps import PCAGuidedTopVoxelsIDIFStep
+
+            ## Initializing the step
+            step = PCAGuidedTopVoxelsIDIFStep(input_image_path='/path/to/image.nii',
+                                              mask_image_path='/path/to/mask.nii',
+                                              output_array_path='/path/to/output.tsv',
+                                              num_pca_components=3,
+                                              verbose=False,
+                                              selected_component=2,
+                                              num_of_voxels=50)
+            ## Running the step and saving the results
+            step()
+
+    """
     def __init__(self,
                  input_image_path: str,
                  mask_image_path: str,
@@ -181,6 +328,18 @@ class PCAGuidedTopVoxelsIDIFStep(ObjectBasedStep, PCAGuidedIDIFMixin):
                  verbose: bool,
                  selected_component: int,
                  num_of_voxels: int):
+        r"""Initializes the PCA-guided top voxels IDIF pipeline step.
+
+        Args:
+            input_image_path (str): Path to the input 4D-PET image.
+            mask_image_path (str): Path to the mask image in the same space as 4D-PET image.
+            output_array_path (str): Path where the resulting TAC data will be saved.
+            num_pca_components (int): Number of PCA components to consider.
+            verbose (bool): If `True`, enables detailed diagnostic output.
+            selected_component (int): PCA component to use for voxel ranking.
+            num_of_voxels (int): Number of top voxels to select.
+
+        """
         PCAGuidedIDIFMixin.__init__(self,
                                     input_image_path=input_image_path,
                                     mask_image_path=mask_image_path,
