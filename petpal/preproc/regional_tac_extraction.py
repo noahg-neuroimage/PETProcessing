15fb1d21242fbe934620394e80bf452ba51f8a6b
noahg-neuroimage <noahg@wustl.edu>
2025-05-05 11:57:01 -0500

    feat: apply_mask_4d can take numpy arrays as an input rather than ants only

--- petpal/preproc/regional_tac_extraction.py
+++ petpal/preproc/regional_tac_extraction.py
@@ -7,8 +7,8 @@ import numpy as np
 from petpal.utils.useful_functions import check_physical_space_for_ants_image_pair
 
 
-def apply_mask_4d(input_image: ants.core.ANTsImage,
-                  mask_image: ants.core.ANTsImage,
+def apply_mask_4d(input_image: ants.core.ANTsImage | np.ndarray,
+                  mask_image: ants.core.ANTsImage | np.ndarray,
                   verbose: bool = False) -> np.ndarray:
     """
     Function to extract ROI voxel tacs from an image using a mask image.
@@ -30,11 +30,19 @@ def apply_mask_4d(input_image: ants.core.ANTsImage,
 
     """
     assert len(input_image.shape) == 4, "Input image must be 4D."
-    assert check_physical_space_for_ants_image_pair(input_image, mask_image), (
-        "Images must have the same physical dimensions.")
+    if isinstance(input_image, ants.ANTsImage) and isinstance(mask_image, ants.ANTsImage):
+        assert check_physical_space_for_ants_image_pair(input_image, mask_image), (
+            "Images must have the same physical dimensions.")
+        input_arr = input_image.numpy()
+        mask_arr = mask_image.numpy()
+    else:
+        assert input_image.shape[:3] == mask_image.shape, (
+            "Images must have the same physical dimensions.")
+        input_arr = input_image.copy()
+        mask_arr = mask_image.copy()
 
-    x_inds, y_inds, z_inds = mask_image.nonzero()
-    out_voxels = input_image.numpy()[x_inds, y_inds, z_inds, :]
+    x_inds, y_inds, z_inds = mask_arr.nonzero()
+    out_voxels = input_arr[x_inds, y_inds, z_inds, :]
     if verbose:
         print(f"(ImageOps): Output TACs have shape {out_voxels.shape}")
     return out_voxels
