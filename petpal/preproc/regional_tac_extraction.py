3755200ac52ca23df76f7fd3241315b016b8429a
noahg-neuroimage <noahg@wustl.edu>
2025-05-05 13:05:21 -0500

    feat: keep original "extract_roi_voxel_tacs_from_image_using_mask" using ANTs but use new "apply_mask_4d" on numpy array internally.

--- petpal/preproc/regional_tac_extraction.py
+++ petpal/preproc/regional_tac_extraction.py
@@ -7,11 +7,43 @@ import numpy as np
 from ..utils.useful_functions import check_physical_space_for_ants_image_pair
 
 
-def apply_mask_4d(input_image: ants.core.ANTsImage | np.ndarray,
-                  mask_image: ants.core.ANTsImage | np.ndarray,
-                  verbose: bool = False) -> np.ndarray:
+def extract_roi_voxel_tacs_from_image_using_mask(input_image: ants.core.ANTsImage,
+                                                 mask_image: ants.core.ANTsImage,
+                                                 verbose: bool = False) -> np.ndarray:
     """
     Function to extract ROI voxel tacs from an image using a mask image.
+
+    This function returns all the voxel TACs, and unlike :func:`extract_mean_roi_tac_from_nifti_using_segmentation`,
+    does not calculate the mean over all the voxels.
+
+    Args:
+        input_image (ants.core.ANTsImage): Input 4D-image from which to extract ROI voxel tacs.
+        mask_image (ants.core.ANTsImage): Mask image which determines which voxels to extract.
+        verbose (bool, optional): If True, prints information about the shape of extracted voxel tacs.
+
+    Returns:
+        out_voxels (np.ndarray): Array of voxel TACs of shape (num_voxels, num_frames)
+
+    Raises:
+         AssertionError: If input image is not 4D-image.
+         AssertionError: If mask image is not in the same physical space as the input image.
+
+    """
+    assert len(input_image.shape) == 4, "Input image must be 4D."
+    assert check_physical_space_for_ants_image_pair(input_image, mask_image), (
+        "Images must have the same physical dimensions.")
+
+    out_voxels = apply_mask_4d(input_arr=input_image.numpy(),
+                               mask_arr=mask_image.numpy(),
+                               verbose=verbose)
+    return out_voxels
+
+
+def apply_mask_4d(input_arr: np.ndarray,
+                  mask_arr: np.ndarray,
+                  verbose: bool = False) -> np.ndarray:
+    """
+    Function to extract ROI voxel tacs from an array using a mask array.
 
     This function applies a 3D mask to a 4D image, returning the time series for each voxel in a
     single flattened numpy array.
@@ -29,17 +61,9 @@ def apply_mask_4d(input_image: ants.core.ANTsImage | np.ndarray,
          AssertionError: If mask image is not in the same physical space as the input image.
 
     """
-    assert len(input_image.shape) == 4, "Input image must be 4D."
-    if isinstance(input_image, ants.ANTsImage) and isinstance(mask_image, ants.ANTsImage):
-        assert check_physical_space_for_ants_image_pair(input_image, mask_image), (
-            "Images must have the same physical dimensions.")
-        input_arr = input_image.numpy()
-        mask_arr = mask_image.numpy()
-    else:
-        assert input_image.shape[:3] == mask_image.shape, (
-            "Images must have the same physical dimensions.")
-        input_arr = input_image.copy()
-        mask_arr = mask_image.copy()
+    assert len(input_arr.shape) == 4, "Input array must be 4D."
+    assert input_arr.shape[:3] == mask_arr.shape, (
+            "Array must have the same physical dimensions.")
 
     x_inds, y_inds, z_inds = mask_arr.nonzero()
     out_voxels = input_arr[x_inds, y_inds, z_inds, :]
