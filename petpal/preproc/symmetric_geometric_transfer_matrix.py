3029290a71699b1661107dcb75872b0eead68ef5
noahg-neuroimage <noahg@wustl.edu>
2025-07-07 14:53:59 -0500

    feat: non-intrusive file names for unidentified subject and session

--- petpal/preproc/symmetric_geometric_transfer_matrix.py
+++ petpal/preproc/symmetric_geometric_transfer_matrix.py
@@ -313,11 +313,14 @@ class Sgtm:
         for i, label in enumerate(self.unique_labels):
             pvc_tac = TimeActivityCurve(times=frame_timing.center_in_mins,
                                         activity=tac_array[i,:])
-            tac_filename = gen_bids_like_filename(sub_id=sub_id,
-                                                  ses_id=ses_id,
-                                                  suffix='tac',
-                                                  seg=int(label),
-                                                  ext='.tsv')
+            if sub_id=='XXXX' or ses_id=='XX':
+                tac_filename = f'seg-{int(label)}_tac.tsv'
+            else:
+                tac_filename = gen_bids_like_filename(sub_id=sub_id,
+                                                    ses_id=ses_id,
+                                                    suffix='tac',
+                                                    seg=int(label),
+                                                    ext='.tsv')
             out_tac_path = os.path.join(out_tac_dir, tac_filename)
             pvc_tac.to_tsv(filename=out_tac_path)
 
