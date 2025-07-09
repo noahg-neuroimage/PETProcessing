f0cb76fde846d8ef95ef60248daa5e356ead28ef
Kenan Oestreich <kenan.oestreich@wustl.edu>
2025-07-09 14:26:03 -0500

    feat(cli_pib_processing.py): add helps

--- petpal/cli/cli_pib_processing.py
+++ petpal/cli/cli_pib_processing.py
@@ -4,7 +4,11 @@ import petpal
 _PIB_EXAMPLE_ = (r"""
 Example:
     - Run a PIB scan through SUVR pipeline:
-      petpal-pib-proc --sub 001 --ses 01 
+      petpal-pib-proc --sub 001 --ses 01 --anat-path ./sub-001/ses-01/anat/sub-001_ses-01_T1w.nii.gz --seg-path ./derivatives/freesurfer/sub-001/ses-01/mri/wmparc.mgz --seg-table-path ./dseg.tsv --ref-region-label 8
+                 
+    - Only compute SUVR for the range of 30-60 min:
+      petpal-pib-proc --sub 001 --ses 01 --anat-path ./sub-001/ses-01/anat/sub-001_ses-01_T1w.nii.gz --seg-path ./derivatives/freesurfer/sub-001/ses-01/mri/wmparc.mgz --seg-table-path ./dseg.tsv --ref-region-label 8 --suvr-start 1800 --suvr-end 3600
+          
 """)
 
 def main():
@@ -13,13 +17,13 @@ def main():
                                      epilog=_PIB_EXAMPLE_, formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('--sub',required=True,help='Subject or participant identifier. Note that this should not include "sub-", but only the value, i.e."--sub 001"')
     parser.add_argument('--ses',required=True,help='Session identifier. Note that this should not include "ses-", but only the value, i.e. "--ses 01"')
-    parser.add_argument('--anat-path',required=True)
-    parser.add_argument('--seg-path',required=True)
-    parser.add_argument('--seg-table-path',required=True)
-    parser.add_argument('--ref-region-label',required=True)
-    parser.add_argument('--suvr-start',required=False)
-    parser.add_argument('--suvr-end',required=False)
-    parser.add_argument('--bids-root',required=False)
+    parser.add_argument('--anat-path',required=True,help='Path to anatomical image')
+    parser.add_argument('--seg-path',required=True,help='Path to segmentation image')
+    parser.add_argument('--seg-table-path',required=True,help='Path to segmentation table file (see /shared/dseg.tsv in petpal for an example)')
+    parser.add_argument('--ref-region-label',required=True,help='Integer label which corresponds to the reference region in the segmentation image')
+    parser.add_argument('--suvr-start',required=False,default=0,help='Time (in seconds) to start SUVR calculation. Defaults to 0 (beginning of scan)')
+    parser.add_argument('--suvr-end',required=False,default=-1,help='Time (in seconds) to end SUVR calculation. Default to -1 (the end of the scan)')
+    parser.add_argument('--bids-root',required=False,default='.',help='Path to root of bids directory. Defaults to current path (".")')
     
     args = parser.parse_args()
 
@@ -27,7 +31,7 @@ def main():
     ses_id = args.ses
     seg_path = args.seg_path
     anat_path = args.anat_path
-    bids_dir = '.' if args.bids_root is None else args.bids_root
+    bids_dir = args.bids_root
     seg_table_path = args.seg_table_path
     suvr_start = args.suvr_start
     suvr_end = args.suvr_end
