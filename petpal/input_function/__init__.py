b28e448c0c4283e89fe8ae23991f649e79b93ad5
noahg-neuroimage <noahg@wustl.edu>
2025-07-22 15:14:43 -0500

    import only PETPAL modules from input_function init

--- petpal/input_function/__init__.py
+++ petpal/input_function/__init__.py
@@ -1,10 +1,15 @@
-from . import blood_input
-from . import idif_necktangle
-
-def main():
-    print("PETPAL - Input Functions")
-
-
-if __name__ == "__main__":
-    main()
-    
+from .blood_input import (extract_blood_input_function_activity_from_csv,
+                          extract_blood_input_function_from_csv,
+                          extract_blood_input_function_times_from_csv,
+                          BloodInputFunction,
+                          resample_blood_data_on_scanner_times,
+                          read_plasma_glucose_concentration)
+from .idif_necktangle import (single_threshold_idif_from_4d_pet_with_necktangle,
+                              average_across_4d_frames,
+                              get_frame_time_midpoints,
+                              load_fslmeants_to_numpy_3d,
+                              double_threshold_idif_from_4d_pet_necktangle)
+from .pca_guided_idif import (PCAGuidedIdifBase,
+                              PCAGuidedTopVoxelsIDIF,
+                              PCAGuidedIdifFitterBase,
+                              PCAGuidedIdifFitter)
