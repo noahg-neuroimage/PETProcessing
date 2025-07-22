a7fc38e33795b497bfc0c8d4b80185e7d677e8a9
noahg-neuroimage <noahg@wustl.edu>
2025-07-22 13:36:32 -0500

    feat: fix imports for preproc modules

--- petpal/preproc/__init__.py
+++ petpal/preproc/__init__.py
@@ -1,16 +1,9 @@
 """Tools for preparing PET data for kinetic modeling and visualization"""
-from . import image_operations_4d
-from . import motion_corr
-from . import partial_volume_corrections
-from . import register
-from . import symmetric_geometric_transfer_matrix
-from . import segmentation_tools
-from . import decay_correction
-from . import regional_tac_extraction
-
-def main():
-    print("PETPAL - Pre-processing")
-
-
-if __name__ == "__main__":
-    main()
+from .image_operations_4d import *
+from .motion_corr import *
+from .partial_volume_corrections import *
+from .register import *
+from .symmetric_geometric_transfer_matrix import *
+from .segmentation_tools import *
+from .decay_correction import *
+from .regional_tac_extraction import *
