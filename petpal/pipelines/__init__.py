0b1881cc406f0132e6d2bf258486fbbd2a4beec2
noahg-neuroimage <noahg@wustl.edu>
2025-07-22 14:06:31 -0500

    fix: pipelines _init_ imports PETPAL modules

--- petpal/pipelines/__init__.py
+++ petpal/pipelines/__init__.py
@@ -1,13 +1,23 @@
-from . import steps_base
-from . import steps_containers
-from . import preproc_steps
-from . import kinetic_modeling_steps
-from . import pca_guided_idif_steps
-from . import pipelines
-
-def main():
-    print("PETPAL - Pipelines")
-
-
-if __name__ == "__main__":
-    main()
+from .kinetic_modeling_steps import (TACAnalysisStepMixin,
+                                     GraphicalAnalysisStep,
+                                     TCMFittingAnalysisStep,
+                                     RTMFittingAnalysisStep,
+                                     ParametricGraphicalAnalysisStep,
+                                     KMStepType)
+from .pca_guided_idif_steps import (PCAGuidedIDIFMixin,
+                                    PCAGuidedFitIDIFStep,
+                                    PCAGuidedTopVoxelsIDIFStep)
+from .pipelines import (BIDSyPathsForRawData,
+                        BIDSyPathsForPipelines,
+                        BIDS_Pipeline)
+from .preproc_steps import (TACsFromSegmentationStep,
+                            ResampleBloodTACStep,
+                            ImageToImageStep,
+                            ImagePairToArrayStep,
+                            PreprocStepType)
+from .steps_base import (ArgsDict,
+                         StepsAPI,
+                         FunctionBasedStep,
+                         ObjectBasedStep)
+from .steps_containers import (StepsContainer,
+                               StepsPipeline)
