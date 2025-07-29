7b71808e362e2feb4f2621637174f8c618f59a4c
Kenan Oestreich <kenan.oestreich@wustl.edu>
2025-07-29 16:13:31 -0500

    Revert "fix: test naive circular import fix"
    This reverts commit 52a2babc851bb77792b7187e68a7888b4f4ecb7f.

--- petpal/utils/__init__.py
+++ petpal/utils/__init__.py
@@ -1,2 +1,18 @@
+from . import bids_utils
+from . import image_io
+from . import math_lib
+from . import testing_utils
+from . import useful_functions
+from . import time_activity_curve
+from . import decorators
+from . import data_driven_image_analyses
+from . import constants
+from . import scan_timing
 
+def main():
+    print("PETPAL - Utilities")
+
+
+if __name__ == "__main__":
+    main()
     
