a4d38e763fb9ad28a098ca265fca43e628f95379
noahg-neuroimage <noahg@wustl.edu>
2025-08-15 13:17:22 -0500

    docs: rename 'model' to 'tac' in certain cases

--- petpal/kinetic_modeling/tac_uncertainty.py
+++ petpal/kinetic_modeling/tac_uncertainty.py
@@ -1,16 +1,16 @@
-"""Tools for calculating uncertainty for application to kinetic models."""
+"""Tools for calculating TAC uncertainty for application to kinetic models."""
 import numpy as np
 
 from ..utils.time_activity_curve import TimeActivityCurve
 
 
-class ModelUncertainty:
+class TacUncertainty:
     """Set uncertainty for kinetic modeling. Includes options for constant,
     calculated, or preset uncertainty.    
     """
     def __init__(self,
                  time_activity_curve: TimeActivityCurve):
-        """Initialize ModelUncertainty with provided arguments.
+        """Initialize TacUncertainty with provided arguments.
 
         Args:
             time_activity_curve (TimeActivityCurve): The time activity curve used to determine
@@ -21,7 +21,7 @@ class ModelUncertainty:
 
     @property
     def constant_uncertainty(self) -> np.ndarray:
-        """Get constant uncertainty for the model.
+        """Get constant uncertainty for the TAC.
         """
         uncertainty = np.ones_like(self.time_activity_curve.activity)
         return uncertainty
@@ -29,7 +29,7 @@ class ModelUncertainty:
 
     @property
     def tac_uncertainty(self) -> np.ndarray:
-        """Get uncertainty stored in the TAC itself for the model.
+        """Get uncertainty stored in the TAC itself to pass on to the kinetic model.
 
         Raises:
             ValueError: If the supplied TAC uncertainty has any NaN values.
@@ -40,14 +40,14 @@ class ModelUncertainty:
         uncertainty_from_tac = self.time_activity_curve.uncertainty
         nan_locations = np.isnan(uncertainty_from_tac)
         if np.any(nan_locations):
-            raise ValueError("Supplied TAC has NaN values in stored uncertianty array. Use a "
-                             "model of uncertainty other than 'tac'.")
+            raise ValueError("Supplied TAC has NaN values in stored uncertianty array. Use an "
+                             "uncertainty method other than 'tac'.")
         return uncertainty_from_tac
 
 
     @property
     def calculated_uncertainty(self):
-        """The calculated uncertainty for the model.
+        """The calculated uncertainty for the TAC.
 
         Currently placeholder function.
         """
@@ -56,14 +56,14 @@ class ModelUncertainty:
 
 
     def __call__(self, uncertainty_method: str='constant') -> np.ndarray:
-        """Get the model uncertainty corresponding to the identified method.
+        """Get the tac uncertainty corresponding to the identified method.
         
         Args:
-            uncertainty_methood (str): model uncertainty type to apply to the model. Default
+            uncertainty_method (str): Uncertainty type to apply to the tac. Default
                 'constant'.
 
         Returns:
-            uncertainty (np.ndarray): The uncertainty applied to each time frame in the model.
+            uncertainty (np.ndarray): The uncertainty applied to each time frame in the TAC.
 
         Raises:
             ValueError: If `uncertainty_method` is not one of: 'constant', 'calculated', or 
