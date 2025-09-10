6d4cc6bd432f3e00ad8fbdbaa0269a88b1062e30
noahg-neuroimage <noahg@wustl.edu>
2025-09-10 09:45:19 -0500

    feat: method for running parametric analysis with reference region linear methods

--- petpal/kinetic_modeling/parametric_images.py
+++ petpal/kinetic_modeling/parametric_images.py
@@ -89,6 +89,71 @@ def apply_linearized_analysis_to_all_voxels(pTAC_times: np.ndarray,
     return slope_img, intercept_img
 
 
+
+@numba.njit()
+def parametric_refregion_analysis(pTAC_times: np.ndarray,
+                                  pTAC_vals: np.ndarray,
+                                  tTAC_img: np.ndarray,
+                                  t_thresh_in_mins: float,
+                                  k2_prime: float,
+                                  analysis_func: Callable) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Generates parametric images for 4D-PET data using the provided analysis method.
+
+    This function iterates over each voxel in the given `tTAC_img` and applies the provided
+    `analysis_func` to compute analysis values. The `analysis_func` should be a numba.jit function
+    for optimization and should be following a signature compatible with either of the following:
+    patlak_analysis, logan_analysis, or alt_logan_analysis.
+
+    Args:
+        pTAC_times (np.ndarray): A 1D array representing the input TAC times in minutes.
+
+        pTAC_vals (np.ndarray): A 1D array representing the input TAC values. This array should
+                                be of the same length as `pTAC_times`.
+
+        tTAC_img (np.ndarray): A 4D array representing the 3D PET image over time.
+                               The shape of this array should be (x, y, z, time).
+
+        t_thresh_in_mins (float): A float representing the threshold time in minutes.
+                                  It is applied when calling the `analysis_func`.
+
+        k2_prime (float): The population averaged reference region k2 value, passed to
+                          `analysis_func`.
+                                  
+        analysis_func (Callable): A numba.jit function to apply to each voxel for given PET data.
+                                  It should take the following arguments:
+
+                                    - input_tac_values: 1D numpy array for input TAC values
+                                    - region_tac_values: 1D numpy array for regional TAC values
+                                    - tac_times_in_minutes: 1D numpy array for TAC times in minutes
+                                    - t_thresh_in_minutes: a float for threshold time in minutes
+                                    - k2_prime: the population averaged reference region k2 value
+                    
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray]: A tuple of two 3D numpy arrays representing the calculated
+            slope image and the intercept image, each of the same spatial dimensions as `tTAC_img`.
+
+    """
+    img_dims = tTAC_img.shape
+
+    slope_img = np.zeros((img_dims[0], img_dims[1], img_dims[2]), float)
+    intercept_img = np.zeros_like(slope_img)
+
+    for i in range(0, img_dims[0], 1):
+        for j in range(0, img_dims[1], 1):
+            for k in range(0, img_dims[2], 1):
+                analysis_vals = analysis_func(input_tac_values=pTAC_vals,
+                                              region_tac_values=tTAC_img[i, j, k, :],
+                                              tac_times_in_minutes=pTAC_times,
+                                              t_thresh_in_minutes=t_thresh_in_mins,
+                                              k2_prime=k2_prime)
+                slope_img[i, j, k] = analysis_vals[0]
+                intercept_img[i, j, k] = analysis_vals[1]
+
+    return slope_img, intercept_img
+
+
 def generate_parametric_images_with_graphical_method(pTAC_times: np.ndarray,
                                                      pTAC_vals: np.ndarray,
                                                      tTAC_img: np.ndarray,
