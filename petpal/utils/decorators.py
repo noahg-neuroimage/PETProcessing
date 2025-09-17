476f0a08c9af2a06dc4c3d24ffa6c1330d66cd3f
Furqan Dar <darf@kenyon.edu>
2025-09-17 10:52:47 -0500

    Revert "Debugging the changes to the decorator"
    This reverts commit 5c51c68cdec8d1a6cdf19c50676e7b01f9ccd4ff.

--- petpal/utils/decorators.py
+++ petpal/utils/decorators.py
@@ -8,7 +8,7 @@ optional path for saving the output of the decorated function.
 
 import functools
 import ants
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
 from ..visualizations.image_visualization import plot_mean_slices_of_img
 
@@ -108,9 +108,8 @@ def ANTsImageToANTsImage(func):
         out_img = func(in_image, *args, **kwargs)
         if out_path is not None:
             ants.image_write(out_img, out_path)
-            if out_path.endswith('.nii.gz'):
-                print("Writing output image")
-                print(type(out_img))
+            # if out_path.endswith('.nii.gz'):
+            #     print("Writing output image")
             #     plot_mean_slices_of_img(out_img, vmin=0, vmax=None)
             #     plt.savefig(out_path.replace('.nii.gz', '.png'),
             #                 bbox_inches='tight', transparent=False, dpi=150)
